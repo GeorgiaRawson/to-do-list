66799fa0bef586b5281f145d338dbec3800050a8
Georgia Rawson <georgiaisabellawatson@gmail.com>
2026-02-02 10:14:12 +0000

    added an alert if no texr entered. now add item needs fixing

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,23 @@
-var addTerm = document.getElementById("addTerm");
-
+var taskInput = document.getElementById("addTask");
 var addButton = document.getElementById("addButton");
+var taskContainer = document.getElementById("tasks");
+
+function onClickAddButton() {
+      if (taskInput.value == "") {
+        alert("Task to be added should not be empty!");
+        return;
+    }
+}
+var newItemEl = document.createElement("div");
+    newItemEl.textContent = taskInput.value;
+
+taskContainer.appendChild(newItemEl);
+taskInput.value="";
 
 addButton.addEventListener("click", onClickAddButton);
 
-function onClickAddButton() {
-    console.log(addTerm.value);
 
-    var newItemEl = document.createElement("div");
-    newItemEl.textContent = addTerm.value;
-    var tasksEl = document.getElementById("tasks");
-    tasksEl.appendChild(newItemEl);
-}
+
+
+
+   
