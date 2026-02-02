4a19a681ec3193868e6d7c20d1e923cd6a7bb707
Georgia Rawson <georgiaisabellawatson@gmail.com>
2026-02-02 11:45:42 +0000

    trying to add edit and delete buttons to JS

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,43 @@ function onClickAddButton() {
       if (taskInput.value == "") {
         alert("Task to be added should not be empty!");
         return;
-    }
-var newItemEl = document.createElement("div");
-    newItemEl.textContent = taskInput.value;
+      }
+var newItemEl = document.createElement("li");
 
-taskContainer.appendChild(newItemEl);
+var checkbox = document.createElement("input");
+checkbox.type="checkbox";
+
+checkbox.onchange = function () {
+    var completedList = document.getElementById("completed-tasks");
+    if (this.checked) { 
+        completedList.appendChild(newItemEL);} 
+        else {
+            taskContainer.appendChild(newItemEL);
+        }
+};
+var label = document.createElement("label");
+label.textContent = taskInput.value; 
+
+var editButton =document.createElement("button");
+editButton.textContent = "Edit"; 
+editButton.onclick = function() {
+    var newText = prompt("Edit task:", label.textContent);
+    if (newText) label.textContent = newText;
+};
+
+ var deleteButton = document.createElement("button");
+ deleteButton.textContent = "Delete";
+ deleteButton.onclick = function () {
+    newItemEl.remove();
+ };
+
+newItemEl.appendChild(checkbox);
+newItemEl.appendChild(label);
+newItemEl.appendChild(editButton);
+newItemEL.appendChild(deleteButton);
+
+taskContainer.appendChild(newItemEl); 
+
 taskInput.value="";
 }
 
